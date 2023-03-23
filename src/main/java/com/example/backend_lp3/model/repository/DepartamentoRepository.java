97079cc7c7fff42297f1311b588867ffa6a679a0
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2023-03-23 07:44:43 -0300

    criando repositories

---
+++ src/main/java/com/example/backend_lp3/model/repository/DepartamentoRepository.java
@@ -0,0 +1,8 @@
+package com.example.backend_lp3.model.repository;
+
+import com.example.backend_lp3.model.entity.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+
+}
