88ec37dcfe665d979e0282559d2fc4889568cde0
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2023-03-28 07:43:24 -0300

    exemplo aula

---
+++ src/main/java/com/example/backend_lp3/api/controller/ClienteController.java
@@ -0,0 +1,4 @@
+package com.example.backend_lp3.api.controller;
+
+public class ClienteController {
+}
---
+++ src/main/java/com/example/backend_lp3/service/ClienteService.java
@@ -0,0 +1,45 @@
+package com.example.backend_lp3.service;
+
+import com.example.backend_lp3.exception.RegraNegocioException;
+import com.example.backend_lp3.model.entity.Cliente;
+import com.example.backend_lp3.model.repository.ClienteRepository;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ClienteService {
+
+    private ClienteRepository repository;
+
+    public ClienteService(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Cliente> getCliente(){
+        return  repository.findAll();
+    }
+
+    public Optional<Cliente> getClienteById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Cliente salvar(Cliente cliente) {
+        //validar(cliente);
+        return repository.save(cliente);
+    }
+
+    @Transactional
+    public void excluir(Cliente cliente) {
+        Objects.requireNonNull(cliente.getId());
+        repository.delete(cliente);
+    }
+
+    public void validar(Cliente cliente) {
+        if (cliente.getNome() == null || cliente.getNome().trim().equals("")) {
+            throw new RegraNegocioException("Nome inválido!");
+        }
+    }
+}
