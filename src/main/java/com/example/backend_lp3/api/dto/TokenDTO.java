58d3533b64c9783f927812faa738c7ea6e3b455b
unknown <marcioa.marcioa@gmail.com>
2023-07-01 02:52:19 -0300

    adicionando security

---
+++ src/main/java/com/example/backend_lp3/api/dto/TokenDTO.java
@@ -0,0 +1,13 @@
+package com.example.backend_lp3.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDTO {
+    private String login;
+    private String token;
+}
--- src/main/java/com/example/backend_lp3/config/SecurityConfig.java
+++ src/main/java/com/example/backend_lp3/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.example.backend_lp3.config;
 
+import com.example.backend_lp3.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 
 @EnableWebSecurity
@@ -16,18 +22,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UsuarioService usuarioService;
 
-    @Autowired
-    private JwtService jwtService;
+//    @Autowired
+//    private JwtService jwtService;
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public OncePerRequestFilter jwtFilter(){
-        return new JwtAuthFilter(jwtService, usuarioService);
-    }
+//    @Bean
+//    public OncePerRequestFilter jwtFilter(){
+//        return new JwtAuthFilter(jwtService, usuarioService);
+//    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
