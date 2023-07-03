d8fdf285e5d0051d9f02cfffb437e3c5a2e3de57
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2023-07-03 18:24:04 -0300

    configurando security e jwt

--- src/main/java/com/example/backend_lp3/config/SecurityConfig.java
+++ src/main/java/com/example/backend_lp3/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.backend_lp3.config;
 
+import com.example.backend_lp3.security.JwtAuthFilter;
+import com.example.backend_lp3.security.JwtService;
 import com.example.backend_lp3.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,86 +16,81 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UsuarioService usuarioService;
 
-//    @Autowired
-//    private JwtService jwtService;
+    @Autowired
+    private JwtService jwtService;
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public OncePerRequestFilter jwtFilter(){
-//        return new JwtAuthFilter(jwtService, usuarioService);
-//    }
+    @Bean
+    public OncePerRequestFilter jwtFilter() {
+        return new JwtAuthFilter(jwtService, usuarioService);
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(usuarioService)
-                .passwordEncoder(passwordEncoder());
+        auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
     }
 
-    @Autowired
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/v1/clientes/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/cores/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/enderecos/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/fornecedores/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/funcionarios/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/generos/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/gerentes/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/metodos-pagamento/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/pedidos/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/produtos/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/produtos-pedido/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/produtos-venda/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/tamanhos/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/usuarios/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/vendas/**")
-                .permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/auth").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/clientes/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/cores/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/enderecos/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/fornecedores/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/funcionarios/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/generos/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/gerentes/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/metodos-pagamento/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/pedidos/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/produtos/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/produtos-pedido/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/produtos-venda/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/tamanhos/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/usuarios/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/vendas/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        ;
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(
-                "/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html",
-                "/webjars/**");
+        web.ignoring()
+                .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+                        "/configuration/security", "/swagger-ui.html", "/webjars/**");
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
--- src/main/java/com/example/backend_lp3/model/repository/UsuarioRepository.java
+++ src/main/java/com/example/backend_lp3/model/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend_lp3.model.repository;
 import com.example.backend_lp3.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
